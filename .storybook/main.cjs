21bfdd67dacf182a0dacfb5d490ce127681c8b8a
Anuar <anuar.neto@petlove.com.br>
2022-10-15 17:41:39 -0300

    fix: remove unnecessary final path

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,10 +14,5 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/design-system'
-    }
   }
 }
